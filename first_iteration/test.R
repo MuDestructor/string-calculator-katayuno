180dc133d1c92fa80452a8f8d3be9afb752a32ae
Fergus Reig Gracia <fergusrg@gmail.com>
2020-02-23 21:38:58 +0100

    Add files via upload

--- first_iteration/test.R
+++ first_iteration/test.R
@@ -3,31 +3,27 @@ library(testthat)
 context("StringCalculatorTests")
 
 stringCalculatorTest <- function(){
-	test_that("empty", {
+	test_that("vacio", {
 		#expect_equivalent(1, 2)
 		stringCalculator = StringCalculator$new()
 		expect_equivalent(stringCalculator$add(""), 0)
-		expect_equivalent(stringCalculator$add("\n"), 0)
 	})
-	test_that("number", {
+	test_that("unnumero", {
 		stringCalculator = StringCalculator$new()
 		expect_equivalent(stringCalculator$add("4"), 4)
+		expect_equivalent(stringCalculator$add("1344"), 1344)
 	})
-	test_that("sum2numers", {
+	test_that("variosnumero", {
 		stringCalculator = StringCalculator$new()
-		expect_equivalent(stringCalculator$add("1,2"), 3)
+		expect_equivalent(stringCalculator$add("1, 2"), 3)
 	})
-	test_that("sumNnumers", {
+	test_that("masvariosnumero", {
 		stringCalculator = StringCalculator$new()
 		expect_equivalent(stringCalculator$add("1,2,3,4,5,6,7,8,9"), 45)
 	})
-	test_that("sumNewSeparator", {
+	test_that("variosnumeroseparador", {
 		stringCalculator = StringCalculator$new()
 		expect_equivalent(stringCalculator$add("1\n2,3"), 6)
-	})
-	test_that("sumCustomSeparator", {
-		stringCalculator = StringCalculator$new()
-		expect_equivalent(stringCalculator$add("//;\n1;2"), 3)
 	})
 }
 stringCalculatorTest()
